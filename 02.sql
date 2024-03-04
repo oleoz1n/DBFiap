66cca55aa398279fa3d0cab489dc0d3bfd7dfd90
oleoz1n <leopity0@gmail.com>
2024-03-04 09:33:42 -0300

    Atualizando o sql 2

--- 02.sql
+++ 02.sql
@@ -1,22 +1,37 @@
 SET SERVEROUTPUT ON;
 
 --Ex1
-variable nome VARCHAR2(255);
+ACCEPT nome CHAR prompt 'Digite seu nome:';
+DECLARE
+txt varchar(100);
 BEGIN
-    :nome := 'Leo';
-    dbms_output.put_line(:nome);
+    txt := '&nome';
+    dbms_output.put_line( 'Meu nome é ' || txt);
 END;
 
 --Ex2
-variable total NUMBER;
-variable vl_real number;
-variable qtd_dolar number;
-
+DECLARE
+    total NUMBER;
+    vl_real NUMBER := 4.99;
+    qtd_dolar number;
 BEGIN
-    :vl_real := 4.99;
-    :qtd_dolar := 3;
-    :total := :qtd_dolar*:vl_real;
-    dbms_output.put_line(:total);
+    qtd_dolar := &qtd;
+    total := qtd_dolar*vl_real;
+    dbms_output.put_line(total);
 END;
 
---Ex3 não entendi
+--Ex3
+DECLARE
+    vl_total number;
+    juros number:=1.01;
+    vl_parc number;
+    qtd_parc number;
+BEGIN
+    vl_total := 500;
+    qtd_parc:= 5;
+    vl_parc := (vl_total/qtd_parc)*juros;
+    dbms_output.put_line('A Quantidade de parcelas: ' || qtd_parc);
+    dbms_output.put_line('O valor total da compra: ' || vl_total);
+    dbms_output.put_line('O valor da parcela: ' || vl_parc);
+    dbms_output.put_line('O valor total a pagar: ' || vl_parc*qtd_parc);
+END;
